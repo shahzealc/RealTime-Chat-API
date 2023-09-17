f99fbb7a59973bc1bd5cbd0daaa208710ec05da9
Zeal Shah <66459953+shahzealc@users.noreply.github.com>
2023-09-17 17:36:32 +0530

    Update dbConnection.js
    Removed Connection string

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-CONNECTION_STRING = "mongodb+srv://zealshah:adminzealshah@cluster0.jq5dvbl.mongodb.net/realtimechatting?retryWrites=true&w=majority"
+CONNECTION_STRING = "CONNECTION STRING OG MONGODB"
 
 const connectDb = async() =>{
 
     try{
         const connect = await mongoose.connect(CONNECTION_STRING);
-        console.log("Hello from dbConnection")
         console.log(connect.connection.host,connect.connection.name);
     }
     catch(err){
